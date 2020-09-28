e1f5a07df88a8ccde1be5cc81274f4bc435568d7
gsy18 <39385677+gsy18@users.noreply.github.com>
2020-09-28 21:47:29 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
-// man.m@media.net
+
 public class Main {
 
     public static void main(String[] args) {
